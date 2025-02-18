40eaa3f70a8336e190d31ffcfc1e8ad9c9d02a3f
Ankan Mahapatra <ankan2526@gmail.com>
2025-02-18 17:30:33 +0530

    Created tools.py | Added 3 tools

---
+++ backend/tools.py
@@ -0,0 +1,41 @@
+## These are tools that will be used by AI Agent to make decisions and answer questions
+
+
+def text_to_sqlite(text: str):
+    """This function will take a text and convert it to a sqlite query
+
+    Args:
+        text (str): The text to be converted to a sqlite query
+
+    Returns:
+        str: The sqlite query
+    """
+    pass
+
+
+
+
+def execute_sqlite_query(conn, query):
+    """This function will execute a sqlite query
+
+    Args:
+        conn (sqlite3.Connection): The connection to the sqlite database
+        query (str): The sqlite query to be executed
+
+    Returns:
+        list: The result of the query
+    """
+    pass
+
+
+
+def search_tool(query: str):
+    """This function will search results from web
+
+    Args:
+        query (str): The query to be searched
+
+    Returns:
+        list: The search results
+    """
+    pass
