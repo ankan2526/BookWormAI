7d9ffdee0313355888f9ff6987b9587687a76e62
Ankan Mahapatra <ankan2526@gmail.com>
2025-03-01 10:52:47 +0530

    Updated UI

--- frontend/bookworm-ai/src/app/components/home/home.component.ts
+++ frontend/bookworm-ai/src/app/components/home/home.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [NgIf, FormsModule, NavbarComponent],
+  imports: [NgIf, NgFor, FormsModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  name: string = '';
-  nameColor: string = 'red';
-  placeholderText: string = 'Enter your name';
-  switch: boolean = false
+  name: String = '';
+  nameColor: String = 'red';
+  placeholderText: String = 'Enter your name';
+  switch: boolean = false;
+  bookList: String[] = ["The Hunger Games", "And Then There Were None", "Atomic Habits"];
+  bookName: String = '';
 
   changeName(name: string) {
     this.name = name;
@@ -23,5 +25,14 @@ export class HomeComponent {
   onClick() {
     if(this.switch) this.switch = false;
     else this.switch = true;
+  }
+
+  add() {
+    if(this.bookName != "") this.bookList.push(this.bookName);
+    this.bookName = '';
+  }
+
+  remove(book: String) {
+    this.bookList = this.bookList.filter(b => b !== book);
   }
 }
