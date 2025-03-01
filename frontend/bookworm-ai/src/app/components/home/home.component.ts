4306ca0f3a8e8ada7178357d582bf68418b5162b
Ankan Mahapatra <ankan2526@gmail.com>
2025-03-01 09:49:48 +0530

    Updated UI

--- frontend/bookworm-ai/src/app/components/home/home.component.ts
+++ frontend/bookworm-ai/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule, NavbarComponent],
+  imports: [NgIf, FormsModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -12,9 +13,15 @@ export class HomeComponent {
   name: string = '';
   nameColor: string = 'red';
   placeholderText: string = 'Enter your name';
+  switch: boolean = false
 
   changeName(name: string) {
     this.name = name;
     console.log(this.name);
+  }
+
+  onClick() {
+    if(this.switch) this.switch = false;
+    else this.switch = true;
   }
 }
